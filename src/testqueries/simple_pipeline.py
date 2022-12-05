2f69acf4343a77e09ade4d9bfc5fc0fac7e03eef
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2022-12-05 13:49:06 +0100

    adds simple pipeline containing python-udfs

---
+++ src/testqueries/simple_pipeline.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sys
+from os import path
+sys.path.append(path.dirname( path.dirname( path.abspath(__file__))))
+from database import create_connection
+
+def commment_len(comment: str) -> int:
+    return str(len(comment))
+
+def order_year(orderdate: str) -> int: 
+    return orderdate[:4]
+
+def order_priority_numeric(orderpriority: str) -> int:
+    return int (orderpriority[0])
+
+df = pd.read_sql("SELECT * FROM udfs.orders_tpch LIMIT 100", create_connection())
+
+df["comment_length"] = df.apply(lambda row : commment_len(row["comment"]), axis=1)
+df["order_year"] = df.apply(lambda row : order_year(row["orderdate"]), axis=1)
+df["order_priority_numeric"] = df.apply(lambda row : order_priority_numeric(row["orderpriority"]), axis=1)
+
+print(df.head())
