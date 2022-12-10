73119ac29afaf65caf3e9ac365f0192fa91e8cd3
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2022-12-10 16:21:37 +0100

    adds arg to only craete desired tables/scalefactors...

--- src/datasetup/setup_tpch.py
+++ src/datasetup/setup_tpch.py
@@ -1,11 +1,10 @@
-import pandas as pd
-import os
 import sys
+import argparse
 import subprocess
 from os import path
 sys.path.append(path.dirname(path.dirname( path.abspath(__file__))))
 from database import psql_connectionstring, databases, TPCH_CREATE_TABLE_COMMAND, TPCH_DROP_TABLE_IF_EXIST_COMMAND
-import argparse
+from typing import List
 
 PSQL_PATH = "C:\\Program Files\\PostgreSQL\\13\\bin\\psql.exe"
 DATA_PATH = "C:\\Users\\Paul\\Desktop\\HPI\\BP\\TPC-H_Tools_v3.0.0\\dbgen\\"
@@ -16,28 +15,41 @@ data_dirs = {
     1 : "1GB"
 }
 
+tpch_scalefactors = data_dirs.keys()
+tpch_tables = ['orders', 'customer', 'lineitem', 'nation', 'partsupp', 'part', 'region', 'supplier']
+
 def execute_sql(statement : str, connstring : str):
     subprocess.call([PSQL_PATH, '-c', statement, connstring])
 
-master_connstring = psql_connectionstring(0)
-# recreate databases
-for sf in data_dirs.keys():
-    dbname = databases[sf]
-    print(f"Creating database {dbname}")
-    execute_sql(f"DROP DATABASE IF EXISTS {dbname};", master_connstring)
-    execute_sql(f"CREATE DATABASE {dbname};", master_connstring)
-    
+def setup(scalefactors : List[float], tables : List[str]):
+    master_connstring = psql_connectionstring(0)
+    # recreate databases doesn't work if other connections are active...
+    for sf in scalefactors:
+        dbname = databases[sf]
+        print(f"Creating database {dbname}")
+        execute_sql(f"DROP DATABASE {dbname} WITH (FORCE);", master_connstring)
+        execute_sql(f"CREATE DATABASE {dbname};", master_connstring)
 
-# fill with tpc-h data
-for sf in data_dirs.keys():
-    db_connstring = psql_connectionstring(sf)
-    subprocess.call([PSQL_PATH, '-c', TPCH_DROP_TABLE_IF_EXIST_COMMAND, db_connstring])
-    print(f"Creating tables for {databases[sf]}")
-    execute_sql(TPCH_CREATE_TABLE_COMMAND, db_connstring)
+    # fill with tpc-h data
+    for sf in scalefactors:
+        db_connstring = psql_connectionstring(sf)
+        subprocess.call([PSQL_PATH, '-c', TPCH_DROP_TABLE_IF_EXIST_COMMAND, db_connstring])
+        print(f"Creating tables for {databases[sf]}")
+        execute_sql(TPCH_CREATE_TABLE_COMMAND, db_connstring)
 
-    tables = ['orders', 'customer', 'lineitem', 'nation', 'partsupp', 'part', 'region', 'supplier']
-    for table in tables: 
-        print(f"{databases[sf]} : {table.capitalize()}")
-        data_path = path.join(DATA_PATH, data_dirs[sf], table + ".tbl")
-        command = "\\copy " + table + " FROM '" + data_path + "' with (format csv, delimiter '|');"
-        execute_sql(command, db_connstring)
+        for table in tables: 
+            print(f"{databases[sf]} : {table.capitalize()}")
+            data_path = path.join(DATA_PATH, data_dirs[sf], table + ".tbl")
+            command = "\\copy " + table + " FROM '" + data_path + "' with (format csv, delimiter '|');"
+            execute_sql(command, db_connstring)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="setup_tpch")
+    parser.add_argument('-sf', '--scalefactor')       
+    parser.add_argument('-t', '--table') 
+
+    args = parser.parse_args()
+    tables : List[str] = [args.table] if args.table else tpch_tables
+    scalefactor : List[float] = [float(args.scalefactor)] if args.scalefactor else tpch_scalefactors
+
+    setup(scalefactor, tables)
