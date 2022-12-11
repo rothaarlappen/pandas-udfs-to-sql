887c8f4cee26146cb279bcb52e2d1f75a9c9d5d1
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2022-12-11 17:15:45 +0100

    rewrote simple pipeline to work on real tpc_h data...

--- src/testqueries/simple_pipeline.py
+++ src/testqueries/simple_pipeline.py
@@ -4,6 +4,7 @@ from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from database import create_connection
 import pandas as pd
+from pandas import Timestamp
 
 conn = create_connection()
 
@@ -12,20 +13,20 @@ def comment_len(comment: str) -> int:
     return str(len(comment))
 
 
-def order_year(orderdate: str) -> int:
-    return orderdate[:4]
+def order_year(orderdate: Timestamp) -> int:
+    return str(orderdate)[:4]
 
 
 def order_priority_numeric(orderpriority: str) -> int:
     return int(orderpriority[0])
 
 
-df = pd.read_sql("SELECT * FROM udfs.orders_tpch LIMIT 100", conn)
+df = pd.read_sql("SELECT * FROM orders", conn)
 
-df["comment_length"] = df.apply(lambda row: comment_len(row["comment"]), axis=1)
-df["order_year"] = df.apply(lambda row: order_year(row["orderdate"]), axis=1)
+df["comment_length"] = df.apply(lambda row: comment_len(row["o_comment"]), axis=1)
+df["order_year"] = df.apply(lambda row: order_year(row["o_orderdate"]), axis=1)
 df["order_priority_numeric"] = df.apply(
-    lambda row: order_priority_numeric(row["orderpriority"]), axis=1
+    lambda row: order_priority_numeric(row["o_orderpriority"]), axis=1
 )
 
 print(df.head())
