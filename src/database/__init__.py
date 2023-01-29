42d07028920f3906aa94ca6b075cfb684a512326
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2023-01-29 23:38:02 +0100

    adds connstring for sqlserver and lowers table/column names as sqlserver creates case sensitive columns/tables by default

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -23,6 +23,7 @@ SQLSERVER_USER = os.getenv("sqlserver_user")
 SQLSERVER_SCALEFACTOR = float(os.getenv("sqlserver_scalefactor") or 0)
 SQLSERVER_SERVER = os.getenv("sqlserver_server")
 
+SQL_FLAVOR = os.getenv("flavor")
 
 def psycopg2_connection_string(scalefactor: int = 0):
     if scalefactor not in DATABASES.keys():
@@ -37,87 +38,87 @@ def psql_connectionstring(scalefactor: int = 0):
     pg_db = DATABASES[scalefactor]
     return f"postgresql://{PG_USER}:{PG_PW}@{PG_HOST}/{pg_db}"
 
-# def execute_sqlcmd(sqlcmd : str, statement : str,  scalefactor: int = 0):
-#     if scalefactor not in DATABASES.keys():
-#         raise ValueError("This TPC-H scalefactor is not supported")
-#     subprocess.call([PSQL_PATH, "-c", statement, connstring])
-
-
-
+def sqlserver_connectionstring(scalefactor: int = 0):
+    if scalefactor not in DATABASES.keys():
+        raise ValueError("This TPC-H scalefactor is not supported")
+    db = DATABASES[scalefactor]
+    return f"mssql+pyodbc://{SQLSERVER_USER}:{SQLSERVER_PW}@{SQLSERVER_HOST}/{db}?driver=SQL+Server+Native+Client+11.0"
 
 def create_connection():
-    return create_engine(psycopg2_connection_string())
-
+    if SQL_FLAVOR == "sqlserver":
+        return create_engine(sqlserver_connectionstring(0.01))    
+    else: 
+        return create_engine(psycopg2_connection_string())
 
 TPCH_CREATE_TABLE_COMMAND = """
-    CREATE TABLE NATION  ( N_NATIONKEY  INTEGER NOT NULL,
-                                N_NAME       CHAR(25) NOT NULL,
-                                N_REGIONKEY  INTEGER NOT NULL,
-                                N_COMMENT    VARCHAR(152));
+   create table nation  ( n_nationkey  integer not null,
+                                n_name       char(25) not null,        
+                                n_regionkey  integer not null,
+                                n_comment    varchar(152));
 
-    CREATE TABLE REGION  ( R_REGIONKEY  INTEGER NOT NULL,
-                                R_NAME       CHAR(25) NOT NULL,
-                                R_COMMENT    VARCHAR(152));
+    create table region  ( r_regionkey  integer not null,
+                                r_name       char(25) not null,        
+                                r_comment    varchar(152));
 
-    CREATE TABLE PART  ( P_PARTKEY     INTEGER NOT NULL,
-                            P_NAME        VARCHAR(55) NOT NULL,
-                            P_MFGR        CHAR(25) NOT NULL,
-                            P_BRAND       CHAR(10) NOT NULL,
-                            P_TYPE        VARCHAR(25) NOT NULL,
-                            P_SIZE        INTEGER NOT NULL,
-                            P_CONTAINER   CHAR(10) NOT NULL,
-                            P_RETAILPRICE DECIMAL(15,2) NOT NULL,
-                            P_COMMENT     VARCHAR(23) NOT NULL );
+    create table part  ( p_partkey     integer not null,
+                            p_name        varchar(55) not null,        
+                            p_mfgr        char(25) not null,
+                            p_brand       char(10) not null,
+                            p_type        varchar(25) not null,        
+                            p_size        integer not null,
+                            p_container   char(10) not null,
+                            p_retailprice decimal(15,2) not null,      
+                            p_comment     varchar(23) not null );      
 
-    CREATE TABLE SUPPLIER ( S_SUPPKEY     INTEGER NOT NULL,
-                                S_NAME        CHAR(25) NOT NULL,
-                                S_ADDRESS     VARCHAR(40) NOT NULL,
-                                S_NATIONKEY   INTEGER NOT NULL,
-                                S_PHONE       CHAR(15) NOT NULL,
-                                S_ACCTBAL     DECIMAL(15,2) NOT NULL,
-                                S_COMMENT     VARCHAR(101) NOT NULL);
+    create table supplier ( s_suppkey     integer not null,
+                                s_name        char(25) not null,       
+                                s_address     varchar(40) not null,    
+                                s_nationkey   integer not null,        
+                                s_phone       char(15) not null,       
+                                s_acctbal     decimal(15,2) not null,  
+                                s_comment     varchar(101) not null);  
 
-    CREATE TABLE PARTSUPP ( PS_PARTKEY     INTEGER NOT NULL,
-                                PS_SUPPKEY     INTEGER NOT NULL,
-                                PS_AVAILQTY    INTEGER NOT NULL,
-                                PS_SUPPLYCOST  DECIMAL(15,2)  NOT NULL,
-                                PS_COMMENT     VARCHAR(199) NOT NULL );
+    create table partsupp ( ps_partkey     integer not null,
+                                ps_suppkey     integer not null,       
+                                ps_availqty    integer not null,       
+                                ps_supplycost  decimal(15,2)  not null,
+                                ps_comment     varchar(199) not null );
 
-    CREATE TABLE CUSTOMER ( C_CUSTKEY     INTEGER NOT NULL,
-                                C_NAME        VARCHAR(25) NOT NULL,
-                                C_ADDRESS     VARCHAR(40) NOT NULL,
-                                C_NATIONKEY   INTEGER NOT NULL,
-                                C_PHONE       CHAR(15) NOT NULL,
-                                C_ACCTBAL     DECIMAL(15,2)   NOT NULL,
-                                C_MKTSEGMENT  CHAR(10) NOT NULL,
-                                C_COMMENT     VARCHAR(117) NOT NULL);
+    create table customer ( c_custkey     integer not null,
+                                c_name        varchar(25) not null,
+                                c_address     varchar(40) not null,
+                                c_nationkey   integer not null,
+                                c_phone       char(15) not null,
+                                c_acctbal     decimal(15,2)   not null,
+                                c_mktsegment  char(10) not null,
+                                c_comment     varchar(117) not null);
 
-    CREATE TABLE ORDERS  ( O_ORDERKEY       INTEGER NOT NULL,
-                            O_CUSTKEY        INTEGER NOT NULL,
-                            O_ORDERSTATUS    CHAR(1) NOT NULL,
-                            O_TOTALPRICE     DECIMAL(15,2) NOT NULL,
-                            O_ORDERDATE      DATE NOT NULL,
-                            O_ORDERPRIORITY  CHAR(15) NOT NULL,
-                            O_CLERK          CHAR(15) NOT NULL,
-                            O_SHIPPRIORITY   INTEGER NOT NULL,
-                            O_COMMENT        VARCHAR(79) NOT NULL);
+    create table orders  ( o_orderkey       integer not null,
+                            o_custkey        integer not null,
+                            o_orderstatus    char(1) not null,
+                            o_totalprice     decimal(15,2) not null,
+                            o_orderdate      date not null,
+                            o_orderpriority  char(15) not null,
+                            o_clerk          char(15) not null,
+                            o_shippriority   integer not null,
+                            o_comment        varchar(79) not null);
 
-    CREATE TABLE LINEITEM ( L_ORDERKEY    INTEGER NOT NULL,
-                                L_PARTKEY     INTEGER NOT NULL,
-                                L_SUPPKEY     INTEGER NOT NULL,
-                                L_LINENUMBER  INTEGER NOT NULL,
-                                L_QUANTITY    DECIMAL(15,2) NOT NULL,
-                                L_EXTENDEDPRICE  DECIMAL(15,2) NOT NULL,
-                                L_DISCOUNT    DECIMAL(15,2) NOT NULL,
-                                L_TAX         DECIMAL(15,2) NOT NULL,
-                                L_RETURNFLAG  CHAR(1) NOT NULL,
-                                L_LINESTATUS  CHAR(1) NOT NULL,
-                                L_SHIPDATE    DATE NOT NULL,
-                                L_COMMITDATE  DATE NOT NULL,
-                                L_RECEIPTDATE DATE NOT NULL,
-                                L_SHIPINSTRUCT CHAR(25) NOT NULL,
-                                L_SHIPMODE     CHAR(10) NOT NULL,
-                                L_COMMENT      VARCHAR(44) NOT NULL);
+    create table lineitem ( l_orderkey    integer not null,
+                                l_partkey     integer not null,
+                                l_suppkey     integer not null,
+                                l_linenumber  integer not null,
+                                l_quantity    decimal(15,2) not null,
+                                l_extendedprice  decimal(15,2) not null,
+                                l_discount    decimal(15,2) not null,
+                                l_tax         decimal(15,2) not null,
+                                l_returnflag  char(1) not null,
+                                l_linestatus  char(1) not null,
+                                l_shipdate    date not null,
+                                l_commitdate  date not null,
+                                l_receiptdate date not null,
+                                l_shipinstruct char(25) not null,
+                                l_shipmode     char(10) not null,
+                                l_comment      varchar(44) not null);
 """
 TPCH_DROP_TABLE_IF_EXIST_COMMAND = """
     DROP TABLE IF EXISTS orders;
