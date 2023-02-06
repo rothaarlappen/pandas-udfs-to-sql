fc957966b0c3496bd9514aa736e6f6f9363d586c
rothaarlappen <37540536+rothaarlappen@users.noreply.github.com>
2023-02-06 14:23:19 +0100

    Adds Sqlserver to benchmark/analysis
    * adds sqlserver data setup code
    
    * adds option to specify sql-flavor
    
    * adds connstring for sqlserver and lowers table/column names as sqlserver creates case sensitive columns/tables by default
    
    * cleanup: uniform connection creation
    
    * adds sqlserver scripts
    
    * wip: starts integration of sqlserver analysis into benchmark
    
    * explain .env setup in readme
    
    * fixing conn-string creation and changing datatype to float
    
    * sqlserver tpch setup uses bcp to work with remotely
    
    * execute sqlserver code in benchmark
    
    * include change requests
    
    * changing sql_flavor before benchmark

--- src/datasetup/setup_tpch.py
+++ src/datasetup/setup_tpch.py
@@ -5,15 +5,19 @@ from os import path
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from database import (
-    psql_connectionstring,
+    connectionstring as psql_connectionstring,
     DATABASES,
     TPCH_CREATE_TABLE_COMMAND,
     TPCH_DROP_TABLE_IF_EXIST_COMMAND,
+    
+    SQLSERVER_HOST, SQLSERVER_PW, SQLSERVER_USER
 )
 from typing import List
 
 PSQL_PATH = "C:\\Program Files\\PostgreSQL\\13\\bin\\psql.exe"
+SQLCMD_PATH = "C:\\Program Files\\Microsoft SQL Server\\Client SDK\\ODBC\\170\\Tools\\Binn\\SQLCMD.EXE"
 DATA_PATH = "C:\\Users\\Paul\\Desktop\\HPI\\BP\\TPC-H_Tools_v3.0.0\\dbgen\\"
+BCP_PATH = "C:\\Program Files\\Microsoft SQL Server\\Client SDK\\ODBC\\170\\Tools\\Binn\\bcp.exe"
 
 DATA_DIRS = {0.01: "10MB", 0.1: "100MB", 1: "1GB", 5: "5GB", 10: "10GB"}
 
@@ -33,9 +37,14 @@ tpch_tables = [
 def execute_sql(statement: str, connstring: str):
     subprocess.call([PSQL_PATH, "-c", statement, connstring])
 
+def execute_sqlserver_sql(statement: str, database: str):
+    subprocess.call([SQLCMD_PATH, "-S", SQLSERVER_HOST, "-U", SQLSERVER_USER, "-P", SQLSERVER_PW, "-d",  database, "-Q", statement])
+
+def execute_sqlserver_bcp(table: str, database: str, file):
+    subprocess.call([BCP_PATH, table, "in", file,  "-S", SQLSERVER_HOST, "-U", SQLSERVER_USER, "-P", SQLSERVER_PW, "-d",  database, "-t", "|", "-c"])
 
 def setup(scalefactors: List[float], tables: List[str]):
-    master_connstring = psql_connectionstring(0)
+    master_connstring = psql_connectionstring(scalefactor=0)
     # recreate databases doesn't work if other connections are active...
     for sf in scalefactors:
         dbname = DATABASES[sf]
@@ -43,16 +52,16 @@ def setup(scalefactors: List[float], tables: List[str]):
         execute_sql(f"DROP DATABASE {dbname} WITH (FORCE);", master_connstring)
         execute_sql(f"CREATE DATABASE {dbname};", master_connstring)
 
-        db_connstring = psql_connectionstring(sf)  # this database should exist now
+        db_connstring = psql_connectionstring(scalefactor=sf)  # this database should exist now
         execute_sql(
             f"CREATE EXTENSION IF NOT EXISTS plpython3u", db_connstring
         )  # we create python extension to allow for python UDFs
 
     # fill with tpc-h data
     for sf in scalefactors:
-        db_connstring = psql_connectionstring(sf)
+        db_connstring = psql_connectionstring(scalefactor=sf)
         subprocess.call(
-            [PSQL_PATH, "-c", TPCH_DROP_TABLE_IF_EXIST_COMMAND, db_connstring]
+            [PSQL_PATH, "-c", TPCH_DROP_TABLE_IF_EXIST_COMMAND(), db_connstring]
         )
         print(f"Creating tables for {DATABASES[sf]}")
         execute_sql(TPCH_CREATE_TABLE_COMMAND, db_connstring)
@@ -68,17 +77,44 @@ def setup(scalefactors: List[float], tables: List[str]):
                 + "' with (format csv, delimiter '|');"
             )
             execute_sql(command, db_connstring)
+# TODO (Paul): refactor both setup-scripts and remove redundancy...
+def setup_sqlserver(scalefactors: List[float], tables: List[str]):
+    default_db = "master"
+    # recreate databases doesn't work if other connections are active...
+    for sf in scalefactors:
+        dbname = DATABASES[sf]
+        print(f"Creating database {dbname}")
+        execute_sqlserver_sql(f"DROP DATABASE {dbname};", default_db)
+        execute_sqlserver_sql(f"CREATE DATABASE {dbname};", default_db)
 
+        execute_sqlserver_sql(
+            f"sp_configure 'external scripts enabled', 1; RECONFIGURE WITH OVERRIDE;", dbname
+        )  # we configure the server to allow for python UDFs
+
+    # fill with tpc-h data
+    for sf in scalefactors:
+        execute_sqlserver_sql(TPCH_DROP_TABLE_IF_EXIST_COMMAND("sqlserver"), DATABASES[sf])
+        print(f"Creating tables for {DATABASES[sf]}")
+        execute_sqlserver_sql(TPCH_CREATE_TABLE_COMMAND, DATABASES[sf])
+
+        for table in tables:
+            print(f"{DATABASES[sf]} : {table.capitalize()}")
+            data_path = path.join(DATA_PATH, DATA_DIRS[sf], table + ".tbl")
+            execute_sqlserver_bcp (table,  DATABASES[sf], data_path)
+    return 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="setup_tpch")
     parser.add_argument("-sf", "--scalefactor")
     parser.add_argument("-t", "--table")
-
+    parser.add_argument("-f", "--flavor")
     args = parser.parse_args()
     tables: List[str] = [args.table] if args.table else tpch_tables
     scalefactor: List[float] = (
         [float(args.scalefactor)] if args.scalefactor else tpch_scalefactors
     )
-
-    setup(scalefactor, tables)
+    print(args)
+    if args.flavor == "sqlserver":
+        setup_sqlserver(scalefactor, tables)
+    else: 
+        setup(scalefactor, tables)
